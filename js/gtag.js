d00f08d05d53a47bbefb8af7c6e5b8d1e281761b
Luis Francisco <luisfranciscoalbertnavarro@gmail.com>
2019-07-16 08:25:06 +0200

    Cambios Analytic
    Cambios Analytic

--- js/gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-async src="https://www.googletagmanager.com/gtag/js?id=UA-90667849-1"
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-90667849-1');
-
