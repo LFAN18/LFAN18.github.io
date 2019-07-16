50d83cbbe7191e9b8f3b887a0438d32612739e3f
Luis Francisco <luisfranciscoalbertnavarro@gmail.com>
2019-07-16 08:16:16 +0200

    Update gtag.js
    Update gtag.js

--- js/gtag.js
+++ js/gtag.js
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-90667849-1"></script>
-<script>
+async src="https://www.googletagmanager.com/gtag/js?id=UA-90667849-1"
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-90667849-1');
-</script>
+
